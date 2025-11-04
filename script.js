d6389a633c254c2ecacaf678e6c3cb0194dd0e5d
aidanbai9 <aidanbai9@gmail.com>
2025-11-04 17:13:02 +0000

    funny

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-date.textContent = time();
-
+initT();
 // global variables/constants
 let score, answer, level;
 const levelArr = document.getElementsByName("level");
@@ -9,12 +8,40 @@ const scoreArr = [];
 playBtn.addEventListener("click", play);
 guessBtn.addEventListener("click", makeGuess);
 
+function initT(){
+    time();
+    interval=setInterval(time,1000);
+}
+
 function time(){
     let d = new Date();
+    let currentHour = d.getHours();
+    let currentMinute = d.getMinutes();
+    let currentSecond = d.getSeconds();
+    if(currentMinute<10){
+        currentMinute="0"+currentMinute;
+    }
+    if(currentSecond<10){
+        currentSecond="0"+currentSecond;
+    }
+    if(currentHour<10){
+        currentHour="0"+currentHour;
+    }
+    console.log(`Current time: ${currentHour}:${currentMinute}:${currentSecond}`);
     // concatenate the date and time
-    d = d.getMonth()+1 + "/" + d.getDate() + "/" + d.getFullYear();
+    let months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let suffix="th";
+    if(d.getDate()%10==1){
+        suffix="st";
+    }else if(d.getDate()%10==2){
+        suffix="nd";
+    }else if(d.getDate()%10==3){
+        suffix="rd";
+    }
+    d = months[d.getMonth()] + " " + d.getDate() + suffix + ", " + d.getFullYear();
     // update here
-    return d;
+    date.textContent = d;
+    curTime.textContent = "Current Time: "+ currentHour + ":"+currentMinute+":"+currentSecond;
 }
 function play(){
     playBtn.disabled = true;
@@ -60,6 +87,32 @@ function reset(){
 }
 
 function updateScore(){
+    console.log(level);
+    if(level==3){
+        if(score<=2){
+            msg.textContent="good score";
+        }else if(score<=4){
+            msg.textContent="mid score";
+        }else{
+            msg.textContent="bad score";
+        }
+    }else if(level==10){
+        if(score<=4){
+            msg.textContent="good score";
+        }else if(score<=7){
+            msg.textContent="mid score";
+        }else{
+            msg.textContent="bad score";
+        }
+    }else{
+        if(score<=7){
+            msg.textContent="good score";
+        }else if(score<=11){
+            msg.textContent="mid score";
+        }else{
+            msg.textContent="bad score";
+        }
+    }
     scoreArr.push(score);
     wins.textContent = "Total wins: "+scoreArr.length;
     let sum=0;
